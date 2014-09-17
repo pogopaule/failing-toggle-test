333d91ec2bb51927ccb21b2e25d24ffd9620a080
fsr <fsr@inxmail.de>
2014-09-17 10:36:09 +0200

    add some more comments

--- app/controllers/index.js
+++ app/controllers/index.js
@@ -1,11 +1,11 @@
 import Ember from 'ember';
 
 export default Ember.Controller.extend({
-  list: [],
+  list: [], // <-- this survives the tests teardown and setup! Why??
 
   actions: {
     add: function() {
-      this.get('list').pushObject('a');
+      this.get('list').pushObject('a'); // <-- is this the wrong way to use arrays in ember?
     }
   }
 });
